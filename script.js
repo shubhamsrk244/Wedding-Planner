e7a0b007d0910c2e0db5a7324a433a8cb6ed8823
shubhamsrk244 <Shubham.2428csit1536@kiet.edu>
2024-12-20 12:28:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('inquiry-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for your inquiry. We will get back to you soon!');
+  });
+  
